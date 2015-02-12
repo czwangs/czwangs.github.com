9be16a8dfe377d794223d1fa3e279c5c1796c587
czwangs <czwang@bogon.mshome.net>
2015-02-12 21:28:56 +0800

    firstTest
    first test

---
+++ 93me/clock.js
@@ -0,0 +1,19 @@
+document.write('<div id="htright" class="float_r align_r"></div>');
+function show_student_time(){ 
+window.setTimeout("show_student_time()", 1000); 
+BirthDay=new Date("2015/02/24");
+today=new Date(); 
+timeold=(BirthDay.getTime()-today.getTime()); 
+sectimeold=timeold/1000 
+secondsold=Math.floor(sectimeold); 
+msPerDay=24*60*60*1000 
+e_daysold=timeold/msPerDay 
+daysold=Math.floor(e_daysold); 
+e_hrsold=(e_daysold-daysold)*24; 
+hrsold=Math.floor(e_hrsold); 
+e_minsold=(e_hrsold-hrsold)*60; 
+minsold=Math.floor((e_hrsold-hrsold)*60); 
+seconds=Math.floor((e_minsold-minsold)*60); 
+	htright.innerHTML='<font  color=#00ffff>When I first met You'+'<br/>'+'I had no idea you would be so important to me';
+	} 
+show_student_time(); 
